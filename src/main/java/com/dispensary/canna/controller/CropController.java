3ee067579e0ec01fd7b4a15af8771a1edb6d6530
Enrique Mejia <mejiaej@gmail.com>
2020-10-04 15:28:16 -0500

    feat: add get crop by id end point

--- src/main/java/com/dispensary/canna/controller/CropController.java
+++ src/main/java/com/dispensary/canna/controller/CropController.java
@@ -1,11 +1,14 @@
 package com.dispensary.canna.controller;
 
 import com.dispensary.canna.entity.Crop;
+import com.dispensary.canna.repository.CropRepository;
 import com.dispensary.canna.service.CropService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -14,8 +17,16 @@ public class CropController {
     @Autowired
     private CropService cropService;
 
+    @Autowired
+    private CropRepository cropRepository;
+
     @GetMapping("/crop")
     public List<Crop> getCrops() {
         return cropService.findAll();
+    }
+
+    @GetMapping("/crop/{id}")
+    public Crop getCrop(@PathVariable Long id) {
+        return cropRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(id.toString()));
     }
 }
